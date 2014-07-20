f030d01eed77255b328047b6757e830266194bf7
Emisvaldo Pereira da Silva <emisvaldo@gmail.com>
2014-07-20 10:06:58 -0300

    ,
    ,

--- src/MediaEscolar.java
+++ src/MediaEscolar.java
@@ -4,6 +4,19 @@
  */
 public class MediaEscolar {
     
-    public static void main(String[] args)
+    public static void main(String[] args){
+        int nota1 = 0;
+        int nota2 = 5;
+        int nota3 = 4;
+        int nota4 = 10;
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        
+        if(media >= 5){
+            System.out.println("Aprovado com média " + media);
+        }else{
+            System.out.println("Reprovado com média " + media);
+        }
+    
+    }
     
 }
